fcaa2ea0fe1bd3e10e54ee38911ed6be34844b03
Kamogelo0404 <kamogelonkadimeng04042gmail.com>
2023-05-24 11:08:55 +0200

    models

---
+++ ModelsProject/ToDOViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ToDOViewController.swift
+//  ModelsProject
+//
+//  Created by DA MAC M1 156 on 2023/05/24.
+//
+
+import UIKit
+
+class ToDOViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
